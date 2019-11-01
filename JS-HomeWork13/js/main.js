80f1c956f187580b3fae2681d49c24195c81aee4
Yevtieiev <supersan@ua.fm>
2019-11-01 11:12:18 +0200

    JS-HomeWork13
    Dark-Light Theme change with JS

--- JS-HomeWork13/js/main.js
+++ JS-HomeWork13/js/main.js
@@ -1,22 +1,24 @@
 window.onload = function ()  {
-    var theme = localStorage.getItem ('theme');
-  if (localStorage.getItem ('theme') ==='dark') {
-    $('.main-table').toggleClass('dark')
+    let theme = localStorage.getItem ('theme');
+    let mainTable=document.querySelectorAll('.main-table')[0];  
+  if (theme ==='dark') {
+      mainTable.classList.add('dark');
    } else {
-   $('.main-table').toggleClass('light')
+      mainTable.classList.add('ligth');
   }
 };
 
-$('.change-theme').on('click', function () {
-    var theme = localStorage.getItem ('theme');
-
+let changeTheme=document.querySelectorAll('.change-theme')[0];
+changeTheme.addEventListener( "click" , function () {
+   let theme = localStorage.getItem ('theme'); 
+    let mainTable=document.querySelectorAll('.main-table')[0];  
   if (theme === 'dark'){
-    $('.main-table').removeClass('dark');
-   $('.main-table').toggleClass('light'); 
+   mainTable.classList.remove('dark');
+   mainTable.classList.toggle('light'); 
    localStorage.setItem('theme','light');
 
   } else if (theme === 'light'){
-    $('.main-table').removeClass('light');
-   $('.main-table').toggleClass('dark');
+   mainTable.classList.remove('light');
+   mainTable.classList.toggle('dark');
       localStorage.setItem('theme','dark');}
   });
